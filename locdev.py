f90d57edb56b6e95d7fab72785b456376521a0ce
pankaj <pankajthekush@gmail.com>
2023-01-15 20:44:41 +0530

    healess

--- locdev.py
+++ locdev.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selc.selc import take_screenshot,convert_64_img
 
 def load_page():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
     driver.get('https://www.brendangregg.com/')
     base_64 = take_screenshot(driver=driver)
     image_file_path = convert_64_img(base_64_str=base_64) 
